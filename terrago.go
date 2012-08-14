daea1a769fa051302df897a3ea33c1e534447795
David Peter <david.a.peter@gmail.com>
2012-08-14 14:42:01 -0400

    Use a WaitGroup.

--- terrago.go
+++ terrago.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 type Grid [][]float64
-
-
 
 func randIter(iter int) float64 {
 	return (rand.Float64()*2.0 - 1.0) * math.Pow(2, 0.8*float64(iter))
@@ -33,64 +32,53 @@ func iterGrid(grid Grid, n int) Grid {
 	oldLen := len(grid)
 	newLen := (oldLen-1)*2 + 1 // must be of form 2**n + 1
 	newGrid := initGrid(newLen)
-  c := make(chan int) // channel to count
-  counter := 0
+	var wg sync.WaitGroup
 
 	// copy over old values
 	for y := 0; y < oldLen; y++ {
 		for x := 0; x < oldLen; x++ {
-			newGrid[2*x][2*y] = grid[x][y]
+			wg.Add(1)
+			go expand(newGrid, grid, x, y, &wg)
 		}
 	}
+
+	wg.Wait()
 
 	// diamond step
 	for y := 1; y < newLen; y += 2 {
 		for x := 1; x < newLen; x += 2 {
-      counter++
-      go diamond(newGrid, x, y, n, c)
-    }
+			wg.Add(1)
+			go diamond(newGrid, x, y, n, &wg)
+		}
 	}
 
-  fmt.Println("A")
-  for {
-    for i := range c {
-      fmt.Println("one i plz?", i)
-      counter -= i
-      if counter == 0 {
-        break
-      }
-    }
-  }
-  fmt.Println("B")
+	wg.Wait()
 
 	// square step
 	for y := 0; y < newLen; y += 2 {
-    for x := 1; x < newLen; x += 2 {
-      counter++
-      go square(newGrid, x, y, n, c)
+		for x := 1; x < newLen; x += 2 {
+			wg.Add(1)
+			go square(newGrid, x, y, n, &wg)
 		}
 	}
 	for y := 1; y < newLen; y += 2 {
 		for x := 0; x < newLen; x += 2 {
-      counter++
-      go square(newGrid, x, y, n, c)
+			wg.Add(1)
+			go square(newGrid, x, y, n, &wg)
 		}
 	}
-  fmt.Println("C")
 
-  for {
-    for i := range c {
-      counter -= i
-      if counter == 0 {
-        break
-      }
-    }
-  }
+	wg.Wait()
 
 	return newGrid
 }
 
-func diamond(grid Grid, x int, y int, n int, c chan int) {
+func expand(newGrid Grid, oldGrid Grid, x int, y int, wg *sync.WaitGroup) {
+	newGrid[2*x][2*y] = oldGrid[x][y]
+	wg.Done()
+}
+
+func diamond(grid Grid, x int, y int, n int, wg *sync.WaitGroup) {
 	var sum, num float64
 	var length int = len(grid)
 	if x-1 >= 0 {
@@ -106,10 +94,10 @@ func diamond(grid Grid, x int, y int, n int, c chan int) {
 		sum, num = sum+grid[x][y+1], num+1
 	}
 	grid[x][y] = sum/num + randIter(n)
-  c <- 1
+	wg.Done()
 }
 
-func square(grid Grid, x int, y int, n int, c chan int) {
+func square(grid Grid, x int, y int, n int, wg *sync.WaitGroup) {
 	var sum, num float64
 	var length int = len(grid)
 	if x-1 >= 0 {
@@ -129,7 +117,7 @@ func square(grid Grid, x int, y int, n int, c chan int) {
 		}
 	}
 	grid[x][y] = sum/num + randIter(n)
-  c <- 1
+	wg.Done()
 }
 
 // Print functions
@@ -168,11 +156,11 @@ func main() {
 	grid := initGrid(3)
 
 	t0 := time.Now()
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 8; i++ {
 		grid = iterGrid(grid, i)
 	}
 	t1 := time.Now()
-  fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
+	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
 
 	// ~ 1.6 secs for n=2 and 10 iterations
 }
