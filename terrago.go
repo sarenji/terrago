244ab36357c5bba4ad954842d9e7ee499f308aeb
David Peter <david.a.peter@gmail.com>
2012-08-14 16:32:57 -0400

    Use channels and finally speed up algorithm.

--- terrago.go
+++ terrago.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
-	"sync"
 	"time"
 )
 
 type Grid [][]float64
+
+const NCPU = 4
 
 func randIter(iter int) float64 {
 	return (rand.Float64()*2.0 - 1.0) * math.Pow(2, 0.8*float64(iter))
@@ -28,57 +29,70 @@ func initGrid(n int) Grid {
 }
 
 // n must be >= 1.
-func iterGrid(grid Grid, n int) Grid {
+func iterGrid(grid Grid, n int, c chan int) Grid {
 	oldLen := len(grid)
 	newLen := (oldLen-1)*2 + 1 // must be of form 2**n + 1
 	newGrid := initGrid(newLen)
-	var wg sync.WaitGroup
 
 	// copy over old values
 	for y := 0; y < oldLen; y++ {
 		for x := 0; x < oldLen; x++ {
-			wg.Add(1)
-			go expand(newGrid, grid, x, y, &wg)
+			expand(newGrid, grid, x, y)
 		}
 	}
-
-	wg.Wait()
 
 	// diamond step
-	for y := 1; y < newLen; y += 2 {
-		for x := 1; x < newLen; x += 2 {
-			wg.Add(1)
-			go diamond(newGrid, x, y, n, &wg)
-		}
+	for i := 0; i < NCPU; i++ {
+		go diamondSegment(newGrid, i, n, c)
 	}
 
-	wg.Wait()
+	for i := 0; i < NCPU; i++ {
+		<-c
+	}
 
 	// square step
-	for y := 0; y < newLen; y += 2 {
-		for x := 1; x < newLen; x += 2 {
-			wg.Add(1)
-			go square(newGrid, x, y, n, &wg)
-		}
-	}
-	for y := 1; y < newLen; y += 2 {
-		for x := 0; x < newLen; x += 2 {
-			wg.Add(1)
-			go square(newGrid, x, y, n, &wg)
-		}
+	for i := 0; i < NCPU; i++ {
+		go squareSegment(newGrid, i, n, c)
 	}
 
-	wg.Wait()
+	for i := 0; i < NCPU; i++ {
+		<-c
+	}
 
 	return newGrid
 }
 
-func expand(newGrid Grid, oldGrid Grid, x int, y int, wg *sync.WaitGroup) {
+func expand(newGrid Grid, oldGrid Grid, x int, y int) {
 	newGrid[2*x][2*y] = oldGrid[x][y]
-	wg.Done()
 }
 
-func diamond(grid Grid, x int, y int, n int, wg *sync.WaitGroup) {
+func diamondSegment(grid Grid, offset int, n int, c chan int) {
+	length := len(grid)
+	for y := 1; y < length; y += 2 {
+		for x := 1 + 2*offset; x < length; x += 2 * NCPU {
+			diamond(grid, x, y, n)
+		}
+	}
+	c <- 1
+}
+
+func squareSegment(grid Grid, offset int, n int, c chan int) {
+	length := len(grid)
+	for y := 0; y < length; y += 2 {
+		for x := 1 + 2*offset; x < length; x += 2 * NCPU {
+			square(grid, x, y, n)
+		}
+	}
+	for y := 1; y < length; y += 2 {
+		for x := 0 + 2*offset; x < length; x += 2 * NCPU {
+			square(grid, x, y, n)
+		}
+	}
+
+	c <- 1
+}
+
+func diamond(grid Grid, x int, y int, n int) {
 	var sum, num float64
 	var length int = len(grid)
 	if x-1 >= 0 {
@@ -94,10 +108,9 @@ func diamond(grid Grid, x int, y int, n int, wg *sync.WaitGroup) {
 		sum, num = sum+grid[x][y+1], num+1
 	}
 	grid[x][y] = sum/num + randIter(n)
-	wg.Done()
 }
 
-func square(grid Grid, x int, y int, n int, wg *sync.WaitGroup) {
+func square(grid Grid, x int, y int, n int) {
 	var sum, num float64
 	var length int = len(grid)
 	if x-1 >= 0 {
@@ -117,7 +130,6 @@ func square(grid Grid, x int, y int, n int, wg *sync.WaitGroup) {
 		}
 	}
 	grid[x][y] = sum/num + randIter(n)
-	wg.Done()
 }
 
 // Print functions
@@ -143,21 +155,22 @@ func prettyPrint(grid Grid) {
 	}
 }
 
-func prettyPrintCompare(grid Grid) {
+func prettyPrintCompare(grid Grid, c chan int) {
 	prevGrid := grid
-	newGrid := iterGrid(prevGrid, 1)
+	newGrid := iterGrid(prevGrid, 1, c)
 	prettyPrint(prevGrid)
 	fmt.Println("-------------------------------------")
 	prettyPrint(newGrid)
 }
 
 func main() {
-	//	prettyPrintCompare(initGrid(9))
+	c := make(chan int, NCPU)
+	//	prettyPrintCompare(initGrid(9), c)
 	grid := initGrid(3)
 
 	t0 := time.Now()
-	for i := 1; i <= 8; i++ {
-		grid = iterGrid(grid, i)
+	for i := 1; i <= 10; i++ {
+		grid = iterGrid(grid, i, c)
 	}
 	t1 := time.Now()
 	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
